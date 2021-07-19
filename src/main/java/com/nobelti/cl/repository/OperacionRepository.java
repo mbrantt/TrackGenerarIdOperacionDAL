8b07898ab0aa612346d12c2c6982cea23d0377a3
mbrantt <mbrantt@nobelti.cl>
2021-07-19 14:39:56 -0400

    Se añade query para traer operaciones por id proyecto

--- src/main/java/com/nobelti/cl/repository/OperacionRepository.java
+++ src/main/java/com/nobelti/cl/repository/OperacionRepository.java
@@ -1,5 +1,7 @@
 package com.nobelti.cl.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,5 +10,6 @@ import com.nobelti.cl.model.Operacion;
 @Repository
 public interface OperacionRepository extends JpaRepository<Operacion, Integer> {
 	public Operacion findTopByOrderByIdOperationDesc();
+	public List<Operacion> findAllByIdProject(int IdProyect);
 	
 }
